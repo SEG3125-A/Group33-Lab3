d96b2e6a32eb4040e594e229e5b5530b815db4dc
Egerm010 <90728977+Egerm010@users.noreply.github.com>
2024-02-02 00:21:42 -0500

    trying image size bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -74,8 +74,8 @@ function populateListProductChoices(slct2) {
 
 		var image = document.createElement('img')
 		image.src = products.find(p => p.name === productName).image;
-		image.width = "200";
-		image.height = "200";
+		image.width = 200;
+		image.height = 200;
 		
 		s3.appendChild(image);
 		
